5c159de9f136a7d279c1e26bef92f1479034397f
Harry <harryk@harryk.dev>
2025-08-29 12:26:05 +0800

    spinal initial commit

---
+++ lib/lexer.ml
@@ -0,0 +1,104 @@
+open Tokens;;
+
+(* TODO: Maybe use array instead of list *)
+
+type lexeme = {
+        tokn: ttype 
+    ;   line: int 
+    ;   colm: int
+} [@@deriving show, eq]
+;;
+
+let mktok line colm tokn = { tokn;  line; colm }
+;;
+
+type lexres = (lexeme, int * int * string) result 
+;;
+
+let isDigit c =
+    c >= '0' && c <= '9'
+;;
+
+let isAlpha c =
+    (c >= 'a' && c <= 'z') ||
+    (c >= 'A' && c <= 'Z') 
+;;
+
+let isAlphaNum c = 
+    isDigit c || isAlpha c 
+;;
+
+let scan_token (line, colm, sstr) index rest =
+    match sstr with
+    | '('  -> Ok ((mktok line colm TLeftParen),  index, rest)
+    | ')'  -> Ok ((mktok line colm TRightParen), index, rest)
+    | ','  -> Ok ((mktok line colm TComma),      index, rest)
+    | '"'  -> Ok ((mktok line colm TQuote),      index, rest)
+    | '.'  -> 
+        (match rest with
+            |  ('.') :: rest' -> 
+                Ok ((mktok line colm TRange), (index+1), rest')
+            | _ -> 
+                Error (line, colm, Format.sprintf "expected range '..'")
+        )
+    | '-'  -> (match rest with
+            | ('>') :: rest' -> 
+                Ok ((mktok line colm TArrow), index+1, rest')
+            | _ -> 
+                Error (line, colm, Format.sprintf "expected einsum arrow function '->'")
+        )
+    |  chr -> (
+        let dbuf = Buffer.create 8 in
+        let () = Buffer.add_char dbuf chr in
+        let drp = ref 0 in
+        if isDigit chr then 
+            let _ = List.take_while (fun c -> 
+                let isd = isDigit c in 
+                if isd then (
+                    incr drp;
+                    Buffer.add_char dbuf c 
+                );
+                isd
+            ) rest in
+            let alp = Buffer.contents dbuf |> int_of_string in
+            Ok ((mktok line colm (TNumeral alp)), index + !drp, (List.drop !drp rest))
+        else if isAlphaNum chr then
+            let _ = List.take_while (fun c -> 
+                let isd = isAlphaNum c in 
+                if isd then (
+                    incr drp;
+                    Buffer.add_char dbuf c;
+                );
+                isd
+            ) rest in
+            let alp = Buffer.contents dbuf in
+            Ok ((mktok line colm (TAlphaNum alp)), (index + !drp), (List.drop !drp rest))
+        else 
+            Error (line, colm, Format.sprintf "unexpected token %c" chr)
+    )
+;;
+
+let (let*) = Result.bind 
+;;
+
+(* run on a single line *)
+let run line pstring = 
+    let rec _tokenize index tseq pstate = 
+        match pstate with 
+        | [] -> 
+            Ok (tseq)
+        | ' ' :: rest -> 
+            _tokenize (index + 1)  (tseq) rest
+        | hd :: rest -> 
+            let* (tok, index', rest') = scan_token (line, index, hd) index rest in
+            _tokenize (index' + 1) (tok :: tseq) rest' 
+    in 
+    pstring
+    |> String.to_seq
+    |> List.of_seq 
+    |> _tokenize 0 []
+    |> (function 
+            | Ok l -> Ok (List.rev l) 
+            | e    -> e
+    )
+;;
---
+++ test/test_spinal_lexer.ml
@@ -0,0 +1,73 @@
+open OUnit;;
+open Spinal;;
+
+let list_prnt l = 
+    let buf = Buffer.create 64 in
+    let ()  = List.iter (
+        Fun.compose (Buffer.add_string buf) (
+            Fun.compose ((^) "\n")
+                (Lexer.show_lexeme)
+        )
+    ) l 
+    in Buffer.contents buf
+;;
+
+let res_prnt p = function 
+    | Ok (o)    -> p o 
+    | Error (_l, _c, s) ->  s
+
+let tests = "Lexer unit tests" >::: [
+    "empty buffer"   >:: (fun _ -> 
+        assert_equal (Ok []) (Lexer.run 0 "") ~printer:(res_prnt (list_prnt))
+    );
+    "empty formulae" >:: (fun _ ->
+        assert_equal (Ok [ 
+            (Lexer.mktok 0 0 Tokens.TLeftParen );
+            (Lexer.mktok 0 1 Tokens.TRightParen);
+        ]) (Lexer.run 0 "()") ~printer:(res_prnt (list_prnt))
+    );
+    "composite tokens" >:: (fun _ -> 
+        assert_equal (Ok [ 
+            (Lexer.mktok 0 0 Tokens.TArrow);
+            (Lexer.mktok 0 3 Tokens.TRange);
+        ]) (Lexer.run 0 "-> ..") ~printer:(res_prnt (list_prnt))
+    );
+    "Tokens with Data" >:: (fun _ -> 
+        assert_equal (Ok [ 
+            (Lexer.mktok 0 0 (Tokens.TAlphaNum "abc"));
+        ]) (Lexer.run 0 "abc") ~printer:(res_prnt (list_prnt))
+    );
+    "Variable" >:: (fun _ ->
+        assert_equal (Ok [ 
+            (Lexer.mktok 0 0 (Tokens.TAlphaNum "D4"));
+            (Lexer.mktok 0 2 (Tokens.TRange));
+            (Lexer.mktok 0 4 (Tokens.TAlphaNum "E5"));
+        ]) (Lexer.run 0 "D4..E5") ~printer:(res_prnt (list_prnt))
+    );
+    "Sample einsum function expression" >:: (fun _ -> 
+        assert_equal (Ok [ 
+            (Lexer.mktok 0 0  (Tokens.TAlphaNum "abc"));
+            (Lexer.mktok 0 3  (Tokens.TComma));
+            (Lexer.mktok 0 4  (Tokens.TAlphaNum "cde"));
+            (Lexer.mktok 0 8  (Tokens.TArrow));
+            (Lexer.mktok 0 11 (Tokens.TAlphaNum "abde"));
+        ]) (Lexer.run 0 "abc,cde -> abde") ~printer:(res_prnt (list_prnt))
+    );
+    "Full unquoted sample einsum summation call with parameters" >:: (fun _ -> 
+        assert_equal (Ok [ 
+            (Lexer.mktok 0 0  (Tokens.TLeftParen));
+            (Lexer.mktok 0 1  (Tokens.TAlphaNum "i"));
+            (Lexer.mktok 0 3  (Tokens.TArrow));
+            (Lexer.mktok 0 6  (Tokens.TComma));
+            (Lexer.mktok 0 8  (Tokens.TAlphaNum "D2"));
+            (Lexer.mktok 0 10 (Tokens.TRange));
+            (Lexer.mktok 0 12 (Tokens.TAlphaNum "D8"));
+            (Lexer.mktok 0 14 (Tokens.TRightParen));
+        ]) (Lexer.run 0 "(i -> , D2..D8)") ~printer:(res_prnt (list_prnt))
+    );
+]
+
+let _ = 
+    run_test_tt_main tests
+;;
+
