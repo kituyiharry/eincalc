4fc85baa62b9227ec0b2a8d697e395525a8432cf
Harry <harryk@harryk.dev>
2025-09-12 18:14:31 +0800

    ndarray interface start

---
+++ lib/ndarray.ml
@@ -0,0 +1,219 @@
+(*
+ *   Copyright (C) Spinal 2025. All Rights Reserved.
+ *   For internal use only. Do not redistribute.
+ *
+ *   Copyright laws and international treaties protect this app. Unauthorized 
+ *   redistribution of this app without express, written permission from our legal 
+ *   department may entail severe civil or criminal penalties.
+ *
+ *)
+module type NDArray = sig 
+    type value
+    type container
+    val  make: int array -> value -> container
+    val  set:  container -> int array -> value -> unit
+    val  get:  container -> int array -> value
+    val  iteri: (int array -> value -> unit) -> container -> unit
+end
+
+module Scalar(Ord: Set.OrderedType): NDArray with type value := Ord.t = struct 
+
+    type value     = Ord.t
+    type container = value ref
+
+    let make (_dims: int array) (v: value) =
+        ref v
+    ;;
+
+    let set (_cont: container) (_dims: int array) (v: value) = 
+        _cont := v
+    ;;
+
+    let get (_cont: container) (_dims: int array) = 
+        !_cont
+    ;;
+
+    let iteri (apply: int array -> value -> unit) _cont =
+        apply [|0|] !_cont
+    ;;
+
+end
+
+(* n * n *)
+module Vector(Ord: Set.OrderedType): NDArray with type value := Ord.t = struct 
+
+    type value     = Ord.t
+    type container = value array
+
+    let make (_dims: int array) (v: value) =
+        let () = assert (Array.length _dims >= 1) in
+        Array.make (Array.unsafe_get _dims 0) v
+    ;;
+
+    let set (_cont: container) (_dims: int array) (v: value) = 
+        Array.unsafe_set _cont (Array.unsafe_get _dims 0) v
+    ;;
+
+    let get (_cont: container) (_dims: int array) = 
+        Array.unsafe_get _cont (Array.unsafe_get _dims 0)
+    ;;
+
+    let iteri (apply: int array -> value -> unit) _cont =
+        Array.iteri (fun i v -> apply [|i|] v) _cont
+    ;;
+
+end
+
+(* n * n *)
+module Matrix(Ord: Set.OrderedType): NDArray with type value := Ord.t = struct 
+
+    type value     = Ord.t
+    type container = value array array
+
+    let make (_dims: int array) (v: value) =
+        let () = assert (Array.length _dims >= 2) in
+        Array.make_matrix (Array.unsafe_get _dims 0) (Array.unsafe_get _dims 1) v
+    ;;
+
+    let set (_cont: container) (_dims: int array) (v: value) = 
+        Array.unsafe_set (Array.unsafe_get _cont (Array.unsafe_get _dims 0)) (Array.unsafe_get _dims 1) v
+    ;;
+
+    let get (_cont: container) (_dims: int array) = 
+        Array.unsafe_get (Array.unsafe_get _cont (Array.unsafe_get _dims 0)) (Array.unsafe_get _dims 1)
+    ;;
+
+    let iteri (apply: int array -> value -> unit) _cont =
+        Array.iteri (fun  i a -> Array.iteri (fun j v -> apply [|i;j|] v) a) _cont
+    ;;
+
+end
+
+(* n * n * n *)
+module BatchMatrix(Ord: Set.OrderedType): NDArray with type value := Ord.t = struct 
+
+    type value     = Ord.t
+    type container = value array array array
+
+    let make (_dims: int array) (v: value) =
+        let () = assert (Array.length _dims >= 3) in
+        Array.init (Array.unsafe_get _dims 2) (fun _ ->
+            Array.make_matrix (Array.unsafe_get _dims 0) (Array.unsafe_get _dims 1) v
+        )
+    ;;
+
+    let set (_cont: container) (_dims: int array) (v: value) = 
+        Array.unsafe_set (
+            Array.unsafe_get (
+                Array.unsafe_get _cont (Array.unsafe_get _dims 0)
+            ) (Array.unsafe_get _dims 1)
+        ) (Array.unsafe_get _dims 2) v
+    ;;
+
+    let get (_cont: container) (_dims: int array) = 
+        Array.unsafe_get (
+            Array.unsafe_get (
+                Array.unsafe_get _cont (Array.unsafe_get _dims 2)
+            ) (Array.unsafe_get _dims 1)
+        ) (Array.unsafe_get _dims 0)
+    ;;
+
+    let iteri (apply: int array -> value -> unit) _cont =
+        Array.iteri (fun  i a -> 
+            Array.iteri (fun j b -> 
+                Array.iteri (fun k v -> 
+                    apply [|i;j;k|] v
+                ) b
+            ) a
+        ) _cont
+    ;;
+
+end
+
+(* only support float dimens at this point *)
+
+module MulDim: NDArray with type value := float = struct 
+    open! Bigarray;;
+
+    type value     = float
+    type container = (float, Bigarray.float64_elt, Bigarray.c_layout) Bigarray.Genarray.t
+
+    (* only upto 16 dimensions! *)
+    let make (_dims: int array) (_v: value) =
+        let () = assert (Array.length _dims >= 1) in
+        Bigarray.Genarray.create Bigarray.Float64 Bigarray.c_layout _dims
+    ;;
+
+    let set = Bigarray.Genarray.set
+    ;;
+
+    let get = Bigarray.Genarray.get
+    ;;
+
+    let iteri (apply: int array -> value -> unit) _cont = 
+        let ndims = Genarray.dims _cont in 
+        let rec iterate_recursive arr indices dims depth =
+            (if depth = Array.length dims then
+                (* Base case: we have all indices, access the element *)
+                  (apply indices @@ Genarray.get arr indices)
+                else
+                    (* Recursive case: iterate through current dimension *)
+                    for i = 0 to dims.(depth) - 1 do
+                        indices.(depth) <- i;
+                        iterate_recursive arr indices dims (depth + 1)
+                    done
+            )
+        in let iterate_genarray_recursive arr =
+            let indices = Array.make (Array.length ndims) 0 in
+            iterate_recursive arr indices ndims 0
+        in iterate_genarray_recursive _cont
+    ;;
+end
+
+
+module MulIntDim: NDArray with type value := float = struct 
+    open! Bigarray;;
+
+    type value     = float
+    type container = (float, Bigarray.float64_elt, Bigarray.c_layout) Bigarray.Genarray.t
+
+    (* only upto 16 dimensions! *)
+    let make (_dims: int array) (_v: value) =
+        let () = assert (Array.length _dims >= 1) in
+        Bigarray.Genarray.create Bigarray.Float64 Bigarray.c_layout _dims
+    ;;
+
+    let set = Bigarray.Genarray.set
+    ;;
+
+    let get = Bigarray.Genarray.get
+    ;;
+
+    let iteri (_apply: int array -> value -> unit) _cont = 
+        let ndims = Genarray.dims _cont in 
+        let rec iterate_recursive arr indices dims depth =
+            (if depth = Array.length dims then
+                (* Base case: we have all indices, access the element *)
+                let value = Genarray.get arr indices in
+                Printf.printf "  [";
+                Array.iteri (fun i idx -> 
+                    Printf.printf "%d" idx;
+                    if i < Array.length indices - 1 then Printf.printf ",") indices;
+                Printf.printf "] = %.0f\n" value
+                else
+                    (* Recursive case: iterate through current dimension *)
+                    for i = 0 to dims.(depth) - 1 do
+                        indices.(depth) <- i;
+                        iterate_recursive arr indices dims (depth + 1)
+                    done
+            )
+        in let iterate_genarray_recursive arr =
+            let indices = Array.make (Array.length ndims) 0 in
+            Printf.printf "Recursive iteration:\n";
+            iterate_recursive arr indices ndims 0
+        in iterate_genarray_recursive _cont
+    ;;
+end
+
+
+
---
+++ test/test_spinal_ndarray.ml
@@ -0,0 +1,44 @@
+open OUnit;;
+open Spinal;;
+
+let _tests = "Ndarray unit tests" >::: [
+    "Scalar initialization" >:: (fun _ -> 
+        let module S = Ndarray.Scalar(Float) in 
+        let s = S.make  [||] 10. in
+        let _ = S.set s [||] 999. in
+        assert_equal 999. (S.get s [||])
+    );
+    "Vector initialization" >:: (fun _ -> 
+        let module S = Ndarray.Vector(Float) in 
+        let s = S.make  [|10|] 10. in
+        let _ = S.set s [|00|] 999. in
+        let _ = S.set s [|09|] 888. in 
+        let () = assert_equal 999. (S.get s [|0|]) in
+        assert_equal 888. (S.get s [|9|])
+    );
+    "Matrix initialization" >:: (fun _ -> 
+        let module S = Ndarray.Matrix(Float) in 
+        let s = S.make  [|3;3|] 10. in
+        let _ = S.set s [|00;00|] 999. in
+        let _ = S.set s [|01;01|] 999. in
+        let _ = S.set s [|02;02|] 999. in
+        let () = assert_equal 999. (S.get s [|0;0|]) in
+        let () = assert_equal 999. (S.get s [|1;1|]) in
+        let () = assert_equal 999. (S.get s [|2;2|]) in
+        assert_equal 999. (S.get s [|0;0|])
+    );
+    "Batch Matrix initialization" >:: (fun _ -> 
+        let module S = Ndarray.BatchMatrix(Float) in 
+        let s = S.make  [|3;3;3|] 10. in
+        let _ = S.set s [|00;00;00|] 999. in
+        let _ = S.set s [|01;01;01|] 999. in
+        let _ = S.set s [|02;02;02|] 999. in
+        let () = assert_equal 999. (S.get s [|0;0;0|]) in
+        let () = assert_equal 999. (S.get s [|1;1;1|]) in
+        let () = assert_equal 999. (S.get s [|2;2;2|]) in
+        assert_equal 999. (S.get s [|0;0;0|])
+    );
+]
+
+let _ = run_test_tt_main _tests
+;;
